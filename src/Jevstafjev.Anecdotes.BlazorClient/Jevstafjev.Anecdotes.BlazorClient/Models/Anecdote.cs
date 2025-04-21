efe17c2cb94937852a5ddba2a383bb4c71de5eb9
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 16:21:05 +0300

    Create models

---
+++ src/Jevstafjev.Anecdotes.BlazorClient/Jevstafjev.Anecdotes.BlazorClient/Models/Anecdote.cs
@@ -0,0 +1,20 @@
+﻿namespace Jevstafjev.Anecdotes.BlazorClient.Models;
+
+public class Anecdote
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public List<Tag> Tags { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public string CreatedBy { get; set; } = null!;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public string? UpdatedBy { get; set; }
+}
---
+++ src/Jevstafjev.Anecdotes.BlazorClient/Jevstafjev.Anecdotes.BlazorClient/Models/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Jevstafjev.Anecdotes.BlazorClient.Models;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int AnecdotesCount { get; set; }
+}
