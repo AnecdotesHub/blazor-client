ada773f42dcfba7c92c3cddb51158361dbf0bd03
Roman-Jevstafjev <roman.jevstafjev@tptlive.ee>
2025-04-21 16:45:04 +0300

    Create Admin page

--- src/Jevstafjev.Anecdotes.BlazorClient/Jevstafjev.Anecdotes.BlazorClient/AppData.cs
+++ src/Jevstafjev.Anecdotes.BlazorClient/Jevstafjev.Anecdotes.BlazorClient/AppData.cs
@@ -2,6 +2,8 @@
 {
     public class AppData
     {
+        public const string AdministratorRoleName = "Administrator";
+
         public const string AnecdoteClientName = "AnecdoteApi";
 
         public const string AnecdoteAuthClientName = "AnecdoteApi.AuthenticationClient";
